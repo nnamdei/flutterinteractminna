d00ce506ca7f5712cd79cc9d43c8296a4e3ef9f7
nnamdi <nnamsok@gmail.com>
2019-12-24 15:17:57 +0100

    image asset include

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -23,6 +23,80 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Text('Sign In',
+                  style: TextStyle(
+                      color: Color.fromRGBO(7, 29, 54, 1),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 32)),
+              Text(
+                  'Endless Partying possibilities awaits you. Kindly fill in your email and password.',
+                  style: TextStyle(
+                      color: Color.fromRGBO(118, 118, 118, 1),
+                      fontWeight: FontWeight.normal,
+                      height: 1.5,
+                      fontSize: 14)),
+              Stack(
+                children: <Widget>[
+                  Text('Email',
+                      style: TextStyle(
+                          color: Color.fromRGBO(87, 87, 87, 1),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12)),
+                  TextFormField()
+                ],
+              ),
+              Stack(
+                children: <Widget>[
+                  Text('Password',
+                      style: TextStyle(
+                          color: Color.fromRGBO(87, 87, 87, 1),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12)),
+                  TextFormField()
+                ],
+              ),
+              Container(
+                child: RaisedButton(
+                  color: Color.fromRGBO(44, 138, 248, 1),
+                  onPressed: () {},
+                  child: Text('Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16)),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Text('Reset Password?',
+                        style: TextStyle(
+                            color: Color.fromRGBO(85, 89, 93, 1),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14)),
+                    Text('Sign Up',
+                        style: TextStyle(
+                            color: Color.fromRGBO(85, 89, 93, 1),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14))
+                  ],
+                ),
+              ),
+              Center(
+                  child: Text('Parte After Parte.',
+                      style: TextStyle(
+                          color: Color.fromRGBO(49, 46, 46, 1),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14)))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -5,6 +5,113 @@ class Onboarding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: 72,
+            child: Text(
+              'V.1.32.1',
+              style: TextStyle(
+                  color: Color.fromRGBO(44, 138, 248, 1),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14),
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color.fromRGBO(44, 138, 248, 0.15)),
+          ),
+          Text('What\'s New?',
+              style: TextStyle(
+                  color: Color.fromRGBO(44, 138, 248, 1),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 36)),
+          ListTile(
+            leading: FittedBox(
+              fit: BoxFit.contain,
+              child: Image.asset('images/bluetooth.png'),
+            ),
+            title: Text('Connect To Multiple Speakers',
+                style: TextStyle(
+                    color: Color.fromRGBO(7, 29, 54, 1),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16)),
+            subtitle: Text(
+                'You can now connect to multiple\nbluetooth speakers at a time.',
+                style: TextStyle(
+                    color: Color.fromRGBO(118, 118, 118, 1),
+                    fontWeight: FontWeight.normal,
+                    height: 1.5,
+                    fontSize: 12)),
+          ),
+          ListTile(
+            leading: FittedBox(
+              fit: BoxFit.contain,
+              child: Image.asset('images/people.png'),
+            ),
+            title: Text('Broadcast Party Venue',
+                style: TextStyle(
+                    color: Color.fromRGBO(7, 29, 54, 1),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16)),
+            subtitle: Text(
+                'Notify friends nearby of an on going party.',
+                style: TextStyle(
+                    color: Color.fromRGBO(118, 118, 118, 1),
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12)),
+          ),
+          ListTile(
+            leading: FittedBox(
+              fit: BoxFit.contain,
+              child: Image.asset('images/smiley.png'),
+            ),
+            title: Text('Flutter Interactive',
+                style: TextStyle(
+                    color: Color.fromRGBO(7, 29, 54, 1),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16)),
+            subtitle: Text(
+                'Run a codelab from your mobile phone and\nshare with the world.',
+                style: TextStyle(
+                    color: Color.fromRGBO(118, 118, 118, 1),
+                    fontWeight: FontWeight.normal,
+                    height: 1.5,
+                    fontSize: 12)),
+          ),
+          
+          Container(
+            height: 48,
+            child: RaisedButton(
+              color: Color.fromRGBO(44, 138, 248, 1),
+              onPressed: (){},
+            child: Text('Sign in ',  style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                height: 1.5,
+                fontSize: 16)),
+            ),
+          ),
+
+          Container(
+            height: 48,
+            decoration: BoxDecoration(
+              border: Border.all(color: Color.fromRGBO(44, 138, 248, 1),width: 1)
+            ),
+            child: RaisedButton(
+              color: Color.fromRGBO(44, 138, 248, 1),
+              onPressed: (){
+                
+              },
+              child: Text('Sign Up',  style: TextStyle(
+                  color: Color.fromRGBO(44, 138, 248, 1),
+                  fontWeight: FontWeight.w500,
+                  height: 1.5,
+                  fontSize: 16)),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
